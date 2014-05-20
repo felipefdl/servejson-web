b4c5731fd39a3f4e9115424fa441713ebec189fc
Felipe F. Lima <felipe@ferreiradelima.com>
2014-05-20 10:58:35 -0300

    changed newrelic key to env variable

--- configuration.js
+++ configuration.js
@@ -4,9 +4,9 @@
 var format  = require('util').format;
 var env     = process.env;
 var configs = {
-    "port"      : env.PORT       || 3002,
-    "newrelic"  : env.NEWRELIC   || false,
-    "api_server": env.API_SERVER || 'http://localhost:3001'
+    "port"       : env.PORT       || 3002,
+    "api_server" : env.API_SERVER || 'http://localhost:3001',
+    "newrelic"   : env.NEWRELIC   || null
 };
 
 module.exports = configs;
